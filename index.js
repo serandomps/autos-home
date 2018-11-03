25a658147eb1fd74fecd20cd827cd44747d2ae76
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-11-03 05:35:15 +0530

    updating

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ dust.loadSource(dust.compile(require('./template'), 'autos-home'));
 module.exports = function (ctx, sandbox, options, done) {
     var ads = {
       photos: [
-          {url: 'https://d1vda6a1j3uyzl.cloudfront.net/images/800x450/53699c34-67c1-4f69-957a-6706ddc4d2fc'},
-          {url: 'https://d1vda6a1j3uyzl.cloudfront.net/images/800x450/622a1155-f2d5-45ed-bad5-c45d55cd7ca6'},
-          {url: 'https://d1vda6a1j3uyzl.cloudfront.net/images/800x450/2b24f696-a85b-4c17-b7f7-5e09b58b2128'},
-          {url: 'https://d1vda6a1j3uyzl.cloudfront.net/images/800x450/261be366-1ef6-4c3d-8efe-fe593148722c'}
+          {url: 'https://d1kdoh4oxh2joj.cloudfront.net/images/800x450/5bd9329d809eef31d6e1e619'},
+          {url: 'https://d1kdoh4oxh2joj.cloudfront.net/images/800x450/5bd9329d809eef31d6e1e618'}
       ]
     };
     dust.render('autos-home', ads, function (err, out) {
