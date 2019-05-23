78eed516f47beba780df44380d69f972eaca06c0
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-05-23 10:44:54 +0530

    updating

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var dust = require('dust')();
 var serand = require('serand');
 var utils = require('utils');
+var Vehicles = require('vehicles').service;
 
 require('gallery');
 
@@ -8,39 +9,46 @@ dust.loadSource(dust.compile(require('./template'), 'autos-home'));
 
 module.exports = function (ctx, container, options, done) {
     var sandbox = container.sandbox;
-    var ads = {
-      photos: [
-          {url: 'https://d1kdoh4oxh2joj.cloudfront.net/images/800x450/5bd9329d809eef31d6e1e619'},
-          {url: 'https://d1kdoh4oxh2joj.cloudfront.net/images/800x450/5bd9329d809eef31d6e1e618'}
-      ]
-    };
-    dust.render('autos-home', ads, function (err, out) {
-        if (err) {
-            return done(err);
-        }
-        sandbox.append(out);
-        done(null, {
-            clean: function () {
-                $('.autos-home', sandbox).remove();
-            },
-            ready: function () {
-                var i;
-                var o = [];
-                var photos = ads.photos;
-                var length = photos.length;
-                var photo;
-                for (i = 0; i < length; i++) {
-                    photo = photos[i];
-                    o.push({
-                        href: photo.url
+    Vehicles.find({
+        sort: {
+            createdAt: -1
+        },
+        count: 2,
+        resolution: '800x450'
+    }, function (err, vehicles) {
+        if (err) return done(err);
+        var ads = {
+            images: [
+                {url: 'https://d1kdoh4oxh2joj.cloudfront.net/images/800x450/5bd9329d809eef31d6e1e619'},
+                {url: 'https://d1kdoh4oxh2joj.cloudfront.net/images/800x450/5bd9329d809eef31d6e1e618'}
+            ]
+        };
+        dust.render('autos-home', vehicles, function (err, out) {
+            if (err) {
+                return done(err);
+            }
+            sandbox.append(out);
+            done(null, {
+                clean: function () {
+                    $('.autos-home', sandbox).remove();
+                },
+                ready: function () {
+                    var o = [];
+                    vehicles.forEach(function (vehicle) {
+                        var images = vehicle._.images || [];
+                        images.forEach(function (image) {
+                            o.push({
+                                href: image.url
+                            });
+                        });
+                    });
+                    blueimp.Gallery(o, {
+                        container: $('.blueimp-gallery-carousel', sandbox),
+                        carousel: true,
+                        stretchImages: true
                     });
                 }
-                blueimp.Gallery(o, {
-                    container: $('.blueimp-gallery-carousel', sandbox),
-                    carousel: true,
-                    stretchImages: true
-                });
-            }
+            });
         });
     });
 };
